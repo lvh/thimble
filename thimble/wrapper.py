860c6753ba71e8598250678421dc009a6dc51538
Laurens Van Houtven <_@lvh.cc>
2014-05-16 12:13:53 +0200

    Add some docstrings

--- thimble/wrapper.py
+++ thimble/wrapper.py
@@ -2,6 +2,9 @@ from functools import partial
 from twisted.internet.threads import deferToThreadPool
 
 class Thimble(object):
+    """A Twisted thread-pool wrapper for a blocking API.
+
+    """
     def __init__(self, reactor, pool, wrapped, blocking_methods):
         self._reactor = reactor
         self._pool = pool
@@ -10,10 +13,21 @@ class Thimble(object):
 
 
     def _deferToThreadPool(self, f, *args, **kwargs):
+        """Defers execution of ``f(*args, **kwargs)`` to the thread pool.
+
+        This returns a deferred which will callback with the result of
+        that expression, or errback with a failure wrapping the raised
+        exception.
+
+        """
         return deferToThreadPool(self._reactor, self._pool, f, *args, **kwargs)
 
 
     def __getattr__(self, attr):
+        """Gets an attribute from the wrapped object, wrapping it to make it
+        asynchronous if necessary.
+
+        """
         value = getattr(self._wrapped, attr)
 
         if attr in self._blocking_methods:
